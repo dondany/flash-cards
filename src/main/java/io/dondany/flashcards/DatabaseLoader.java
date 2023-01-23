676b50ff6285bdf8f99baf49d9edb38f9afd2890
Daniel Ziętala <daniel.zietala@ttpsc.pl>
2023-01-23 10:19:14 +0100

    Backend - added some test data

--- src/main/java/io/dondany/flashcards/DatabaseLoader.java
+++ src/main/java/io/dondany/flashcards/DatabaseLoader.java
@@ -10,16 +10,27 @@ public class DatabaseLoader {
     @Bean
     CommandLineRunner initDatabase(FlashCardRepository repository) {
         return args -> {
-          repository.save(new FlashCard("być", "essere"));
-          repository.save(new FlashCard("mieć","avere"));
-          repository.save(new FlashCard("móc","potere"));
-          repository.save(new FlashCard("robić","fare"));
-          repository.save(new FlashCard("mówić","dire"));
-          repository.save(new FlashCard("przyjść","venire"));
-          repository.save(new FlashCard("musieć","dovere"));
-          repository.save(new FlashCard("dawać","dare"));
-          repository.save(new FlashCard("iść","andare"));
-          repository.save(new FlashCard("widzieć","vedere"));
+            repository.save(new FlashCard("być", "essere"));
+            repository.save(new FlashCard("mieć", "avere"));
+            repository.save(new FlashCard("móc", "potere"));
+            repository.save(new FlashCard("robić", "fare"));
+            repository.save(new FlashCard("mówić", "dire"));
+            repository.save(new FlashCard("przyjść", "venire"));
+            repository.save(new FlashCard("musieć", "dovere"));
+            repository.save(new FlashCard("dawać", "dare"));
+            repository.save(new FlashCard("iść", "andare"));
+            repository.save(new FlashCard("widzieć", "vedere"));
+
+            repository.save(new FlashCard("żyć", "mieszkać"));
+            repository.save(new FlashCard("rozumieć", "capire"));
+            repository.save(new FlashCard("przybywać", "arrivare"));
+            repository.save(new FlashCard("czekać", "aspettare"));
+            repository.save(new FlashCard("pić", "bere"));
+            repository.save(new FlashCard("pytać", "chiedere"));
+            repository.save(new FlashCard("zaczynać", "cominciare"));
+            repository.save(new FlashCard("kupować", "comprare"));
+            repository.save(new FlashCard("znać", "conoscere"));
+            repository.save(new FlashCard("kończyć", "finire"));
         };
     }
 
