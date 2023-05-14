e491c8af3d96a7c4a2773a3d54933d5eaba65381
Daniel Ziętala <daniel.zietala@ttpsc.pl>
2023-05-14 14:31:01 +0200

    Added relationship project->user and moved data init to sql

--- src/main/java/io/dondany/fc/project/Project.java
+++ src/main/java/io/dondany/fc/project/Project.java
@@ -2,6 +2,7 @@ package io.dondany.fc.project;
 
 import io.dondany.fc.collection.Collection;
 
+import io.dondany.fc.user.User;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -9,6 +10,8 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -33,6 +36,10 @@ public class Project {
 
     @OneToMany(mappedBy = "project", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private List<Collection> collections = new ArrayList<>();
+
+    @ManyToOne
+    @JoinColumn(name="user_id")
+    private User user;
 
     public void addCollection(Collection collection) {
         collections.add(collection);
---
+++ src/main/resources/data.sql
@@ -0,0 +1,44 @@
+--harry.potter@hgwrts.com/pass
+INSERT INTO "_user"
+(id, email, firstname, lastname, "password", "role")
+VALUES(1, 'harry.potter@hgwrts.com', 'harry', 'potter', '$2a$10$P6G7ztCfBQ.zN0f4SQuSLexql1DicStxH2gb/MeyX7dEVmtmbTEaO', 'USER');
+
+---------- PROJECTS ------------
+INSERT INTO public.project (id,description,"name",user_id) VALUES
+                                                               (1,'L''italiano e'' fantastico!','Italiano',1),
+                                                               (2,'Mastering javascript','JavaScript',1),
+                                                               (3,'Mastering Java','Java',1),
+                                                               (4,'Mastering GO','Go',1);
+
+
+---------- Collections ------------
+INSERT INTO public.collection (id,description,"name",project_id) VALUES
+                                                                     (1,'Słówka z pierwszej lekcji','Lezione 1',1),
+                                                                     (2,'Słówka z lekcji 2.','Lezione 2',1),
+                                                                     (3,'Słówka z lekcji 3.','Lezione 3',1),
+                                                                     (4,'Czasowniki','I verbi',1);
+
+
+---------- Flashcards ------------
+INSERT INTO public.flash_card (id,back,front,collection_id) VALUES
+                                                                (1,'essere','być',1),
+                                                                (2,'avere','mieć',1),
+                                                                (3,'potere','móc',1),
+                                                                (4,'fare','robić',1),
+                                                                (5,'dire','mówić',1),
+                                                                (6,'venire','przyjść',1),
+                                                                (7,'dovere','musieć',1),
+                                                                (8,'dare','dawać',1),
+                                                                (9,'andare','iść',1),
+                                                                (10,'vedere','widzieć',1);
+INSERT INTO public.flash_card (id,back,front,collection_id) VALUES
+                                                                (11,'mieszkać','żyć',1),
+                                                                (12,'capire','rozumieć',1),
+                                                                (13,'arrivare','przybywać',1),
+                                                                (14,'aspettare','czekać',1),
+                                                                (15,'bere','pić',1),
+                                                                (16,'chiedere','pytać',1),
+                                                                (17,'cominciare','zaczynać',1),
+                                                                (18,'comprare','kupować',2),
+                                                                (19,'conoscere','znać',2),
+                                                                (20,'finire','kończyć',2);
