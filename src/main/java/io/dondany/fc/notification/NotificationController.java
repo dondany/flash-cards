9d1cfda566b1d6b6d5c04880cbfe6dc46a10b759
Daniel Ziętala <daniel.zietala@ttpsc.pl>
2023-05-27 14:33:32 +0200

    Backend - refactored imports

--- src/main/java/io/dondany/fc/notification/NotificationController.java
+++ src/main/java/io/dondany/fc/notification/NotificationController.java
@@ -5,7 +5,6 @@ import io.dondany.fc.user.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
