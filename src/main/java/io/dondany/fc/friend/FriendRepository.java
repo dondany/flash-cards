321774ea12f66527fa88e22253b56d50225f73d4
dany <danielzietala@gmail.com>
2023-06-16 16:42:14 +0200

    Backend - Fixed query for authenticated user's friends

--- src/main/java/io/dondany/fc/friend/FriendRepository.java
+++ src/main/java/io/dondany/fc/friend/FriendRepository.java
@@ -2,11 +2,20 @@ package io.dondany.fc.friend;
 
 import io.dondany.fc.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface FriendRepository extends JpaRepository<Friend, Long> {
-    List<Friend> findAllByFriendOne(User user);
+
+    /**
+     * Finds all friends for two users
+     *
+     * @param userOne friendOne role
+     * @param userTwo friendTwo role
+     * @return friends
+     */
+    List<Friend> findAllByFriendOneOrFriendTwo(User userOne, User userTwo);
 }
