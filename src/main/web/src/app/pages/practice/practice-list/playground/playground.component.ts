379b12ed077d2cb874265e05ed4283ae7c42b635
dany <danielzietala@gmail.com>
2023-07-08 14:03:00 +0200

    Frontend - Work on the basic playground

--- src/main/web/src/app/pages/practice/practice-list/playground/playground.component.ts
+++ src/main/web/src/app/pages/practice/practice-list/playground/playground.component.ts
@@ -1,6 +1,5 @@
 import {Component, OnInit} from '@angular/core';
 import {PracticeService} from "../../../../shared/services/practice-service";
-import {PracticeType} from "../../../../shared/types/practice-type";
 import {FlashCardType} from "../../../project/types/flash-card-type";
 import {ActivatedRoute} from "@angular/router";
 
@@ -10,9 +9,8 @@ import {ActivatedRoute} from "@angular/router";
   styleUrls: ['./playground.component.scss']
 })
 export class PlaygroundComponent implements OnInit {
-  // practice: PracticeType;
   flashCards!: FlashCardType[];
-  currentFlashCard!: FlashCardType;
+  currentIndex = 0;
   isFlipped: boolean = false;
 
   constructor(private practiceService: PracticeService,
@@ -23,20 +21,46 @@ export class PlaygroundComponent implements OnInit {
     const practiceId = this.activatedRoute.snapshot.params['id'];
     this.practiceService.getFlashCards(practiceId)
       .subscribe((flashCards) => {
-        this.flashCards = flashCards;
-        let random = Math.floor(Math.random() * (flashCards.length))
-        this.currentFlashCard = this.flashCards[random];
+        this.flashCards = this.shuffle(flashCards);
+        this.currentIndex = 0;
       })
+  }
 
+  get currentFlashCard() {
+    return this.flashCards[this.currentIndex];
+  }
 
+  get isFirst() {
+    return this.currentIndex === 0;
+  }
+
+  get isLast() {
+    return this.currentIndex + 1 === this.flashCards.length;
+  }
+
+  prev() {
+    if (!this.isFirst) {
+      this.currentIndex--;
+      this.isFlipped = false;
+    }
   }
 
   next() {
-    let random = Math.floor(Math.random() * (this.flashCards.length))
-    this.currentFlashCard = this.flashCards[random];
+    if (!this.isLast) {
+      this.currentIndex++;
+      this.isFlipped = false;
+    }
   }
 
   flip() {
     this.isFlipped = !this.isFlipped;
+  }
+
+  shuffle(array: FlashCardType[]) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
   }
 }
