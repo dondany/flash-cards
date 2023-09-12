0d131e810e3304db59811dd0d778b6afd868afa8
dany <danielzietala@gmail.com>
2023-09-12 22:18:25 +0200

    Added configuration for docker

--- src/main/java/io/dondany/fc/FlashCardsApplication.java
+++ src/main/java/io/dondany/fc/FlashCardsApplication.java
@@ -8,6 +8,7 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
 import java.util.Arrays;
+import java.util.Collections;
 
 @SpringBootApplication
 public class FlashCardsApplication {
@@ -20,7 +21,7 @@ public class FlashCardsApplication {
 	public CorsFilter corsFilter() {
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
 		corsConfiguration.setAllowCredentials(true);
-		corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+		corsConfiguration.setAllowedOriginPatterns(Collections.singletonList("*"));
 		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
 				"Accept", "Authorization", "Origin, Accept", "X-Requested-With",
 				"Access-Control-Request-Method", "Access-Control-Request-Headers"));
